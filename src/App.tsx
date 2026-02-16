8e036e6cf6d579f47a515c98315f5be0bf5a4c7c
ThanapatrP <thanapatr.pul@gmail.com>
2026-02-16 22:27:02 +0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p className="bg-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit animi
+        nobis corporis porro quo officia nisi laboriosam consequuntur
+        cupiditate! Provident soluta at neque. Ipsam soluta adipisci cum
+        voluptas aut id.
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
