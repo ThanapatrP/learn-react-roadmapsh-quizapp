a65b96155a0f0789fa16f8585bfb1c94afde36d4
ThanapatrP <thanapatr.pul@gmail.com>
2026-03-05 14:19:27 +0700

    test - read json file

--- src/comps/QuizCard.tsx
+++ src/comps/QuizCard.tsx
@@ -1,16 +1,23 @@
+import { useState, useEffect } from "react";
 import "./QuizCard.css";
 
 function QuizCard() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('/src/data/test.json')
+      .then(response => response.json())
+      .then(data => setData(data.name))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="m-3 p-3 bg-white rounded-xl">
       <div className="mb-2 ">
         <h2 className="text-sm underline decoration-1">Question #1</h2>
         <h1 className="text-xl font-bold text-blue-700">?????</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ipsa
-          dolorum vero iusto non nulla praesentium odit voluptates. At repellat
-          facilis aliquid aspernatur incidunt ipsa obcaecati commodi laborum
-          enim eum?
+          {data}
         </p>
       </div>
       <div className="grid grid-cols-2">
